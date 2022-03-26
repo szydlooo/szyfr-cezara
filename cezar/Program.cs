e069c49675cbea7175776022a9e9d2d6674d3e2c
oem <oem@DESKTOP-P9U1V2P>
2022-03-26 01:00:11 +0100

    Dodaj pliki projektów.

---
+++ cezar/Program.cs
@@ -0,0 +1,13 @@
+﻿string tekst;
+Console.WriteLine("Wiadomość");
+tekst = Console.ReadLine();
+int przesunięcie;
+Console.WriteLine("przesunięcie");
+przesunięcie = Int32.Parse(Console.ReadLine());
+char[] tab = tekst.ToCharArray();
+for (int i = 0; i < tab.Length; i++)
+{
+    tab[i] = (char)((char)tab[i] + przesunięcie);
+}
+string szyfr = new string(tab);
+Console.WriteLine("Zaszyfrowany kod to: " + szyfr);
